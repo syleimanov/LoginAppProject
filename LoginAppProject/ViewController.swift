25eb1cd358b71e569eb9698957db0996f9d0b12b
VLADIMIR SYLEIMANOV <bannabels@gmail.com>
2020-12-11 11:55:20 +0300

    Initial Commit

---
+++ LoginAppProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginAppProject
+//
+//  Created by Vladimir Syleimanov on 11.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
